4d54908d223e04a8ee32f89ba2f6be704e0c1e40
shyonae <scionae@gmail.com>
2024-06-23 17:33:49 +0200

    don't remember

--- scripts/start_all.sh
+++ scripts/start_all.sh
@@ -1,5 +1,6 @@
 #/bin/bash
 
 for compose in $(find "$PWD/../" -type f -not -path '*.unused*' -name 'compose.yml' | sort); do
-  infisical run -- docker compose -f $compose up -d
+  	infisical run -- docker compose pull
+	infisical run -- docker compose -f $compose up -d
 done
