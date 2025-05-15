f78ec40980869813a15b209192029a20a8da05e4
shyonae <scionae@gmail.com>
2025-05-15 21:45:15 +0200

    feat: moved containers around, removed t440

--- hosts/t440p_1/scripts/down_all.sh
+++
@@ -1,5 +0,0 @@
-#/bin/bash
-
-for compose in $(find "$PWD/../composes" -type f -not -path '*.unused*' -name 'compose.yml' | sort -r); do
-  docker compose --env-file /smb/backups/docker-server-env/.env -f $compose down
-done
